75f3d5005204e2c48464e741df7bf3bbb138c0e7
Willi Eggeling <email@wje-online.de>
2014-02-18 10:16:24 +0100

    actual class output is now written

--- language-libraries/dryad/src/org/sugarj/dryad/strategies/WriteClassFile_0_1.java
+++ language-libraries/dryad/src/org/sugarj/dryad/strategies/WriteClassFile_0_1.java
@@ -1,7 +1,10 @@
 package org.sugarj.dryad.strategies;
 
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import org.spoofax.interpreter.terms.IStrategoTerm;
 import org.spoofax.interpreter.terms.ITermFactory;
+import org.spoofax.terms.StrategoString;
 import org.strategoxt.lang.Context;
 import org.strategoxt.lang.Strategy;
 
@@ -11,10 +14,21 @@ public class WriteClassFile_0_1 extends Strategy{
 	public static WriteClassFile_0_1 instance = new WriteClassFile_0_1();
 	
 	@Override
-	public IStrategoTerm invoke(Context context, IStrategoTerm current, IStrategoTerm t1) {
+	public IStrategoTerm invoke(Context context, IStrategoTerm current, IStrategoTerm t_path) {
 	  ITermFactory factory = context.getFactory();
-	  //factory.makeString(arg0)
-	  //context.getIOAgent().printError(error)
+	  try{
+	    //TODO: do actual class-file conversion
+	    String path = ((StrategoString)t_path).stringValue();
+	    FileOutputStream out = new FileOutputStream(path);
+	    PrintStream pp = new PrintStream(out);
+	    pp.print(current);
+	    pp.close();
+	    out.close();
+	  }catch(Exception ex){
+	    context.getIOAgent().printError(ex.getMessage());
+	    //TODO: Error handling
+	    return factory.makeInt(1);
+	  }
 	  return factory.makeInt(0);
 	}
 	
